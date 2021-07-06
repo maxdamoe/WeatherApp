2342c8eb6ceb100e53215b80eb2a29b2dbfcd408
msimp <max.simpson@gmail.com>
2021-07-05 20:58:56 -0500

    switching to mac

--- Assets/index.js
+++ Assets/index.js
@@ -1,4 +1,4 @@
-
+1
 
 
 
@@ -9,7 +9,7 @@
 //     "apiKey": b5ab120171c8236c0b40722895f5f571
 // }
 function callAPI(){
-    fetch('api.openweathermap.org/data/2.5/weather?q=Chicago&units=imperial&appid=b5ab120171c8236c0b40722895f5f571')
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=Chicago&units=imperial&appid=b5ab120171c8236c0b40722895f5f571')
     .then((response) => response.json())
     .then((data) => console.log(data))
 }
@@ -17,3 +17,18 @@ function callAPI(){
 callAPI ()
 console.log("yolo")
 // b5ab120171c8236c0b40722895f5f571 --> API KEY FOR WEATHER APP
+
+
+//getting the city name 
+
+
+let button = document.getElementById("button")
+
+button.addEventListener('click', () => searchCity(), () => console.log('worked'))
+
+let City = document.getElementById("CCITY").innerHTML
+
+function searchCity(){
+    
+    console.log(City)
+}
